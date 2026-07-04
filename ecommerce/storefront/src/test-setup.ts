2e9fe5b7083ccbbcb7512cbd4d77088506cee45a
zaidharissheikh <zaidsaigal2005@gmail.com>
2026-07-04 13:18:45 +0500

    final fixx

--- ecommerce/storefront/src/test-setup.ts
+++ ecommerce/storefront/src/test-setup.ts
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
@@ -6,10 +7,10 @@ Object.defineProperty(window, 'matchMedia', {
     matches: false,
     media: query,
     onchange: null,
-    addListener: () => {},
-    removeListener: () => {},
-    addEventListener: () => {},
-    removeEventListener: () => {},
-    dispatchEvent: () => false,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(() => false),
   }),
 });
